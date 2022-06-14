b2ce023a57ccc9142dd4a9ce46903607d8a68626
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2022-06-14 11:29:12 -0500

    0.2.0 beta release

--- fabric/src/main/java/com/epherical/professions/ProfessionsFabric.java
+++ fabric/src/main/java/com/epherical/professions/ProfessionsFabric.java
@@ -70,7 +70,7 @@ public class ProfessionsFabric implements ModInitializer {
         this.config.loadConfig();
         if (ProfessionConfig.useBuiltinDatapack) {
             ResourceManagerHelper.registerBuiltinResourcePack(new ResourceLocation("professions", "normal"),  FabricLoader.getInstance().getModContainer("professions").get(), ResourcePackActivationType.DEFAULT_ENABLED);
-            ResourceManagerHelper.registerBuiltinResourcePack(new ResourceLocation("professions", "hardcore"),  FabricLoader.getInstance().getModContainer("professions").get(), ResourcePackActivationType.DEFAULT_ENABLED);
+            //ResourceManagerHelper.registerBuiltinResourcePack(new ResourceLocation("professions", "hardcore"),  FabricLoader.getInstance().getModContainer("professions").get(), ResourcePackActivationType.DEFAULT_ENABLED);
         }
 
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {

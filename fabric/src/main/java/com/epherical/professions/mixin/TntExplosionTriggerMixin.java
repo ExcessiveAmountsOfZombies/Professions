d9648b3f6742438d3a6f5a1191839e99a9e8c82f
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2022-06-20 12:07:13 -0500

    fix local capture 1.19 only

--- fabric/src/main/java/com/epherical/professions/mixin/TntExplosionTriggerMixin.java
+++ fabric/src/main/java/com/epherical/professions/mixin/TntExplosionTriggerMixin.java
@@ -1,14 +1,12 @@
 package com.epherical.professions.mixin;
 
 import com.epherical.professions.events.trigger.TriggerEvents;
-import com.mojang.datafixers.util.Pair;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectListIterator;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Explosion;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,8 +15,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.Iterator;
 
 @Mixin(Explosion.class)
 public abstract class TntExplosionTriggerMixin {
@@ -29,7 +25,7 @@ public abstract class TntExplosionTriggerMixin {
 
     @Inject(method = "finalizeExplosion", locals = LocalCapture.CAPTURE_FAILHARD,
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"))
-    public void onExploded(boolean spawnParticles, CallbackInfo ci, boolean bl, ObjectArrayList<Pair<ItemStack, BlockPos>> objectArrayList, Iterator var4, BlockPos blockPos, BlockState blockState, Block block) {
+    public void onExploded(boolean spawnParticles, CallbackInfo ci, boolean blz, ObjectArrayList list, boolean bl, ObjectListIterator iterator, BlockPos blockPos, BlockState blockState) {
         if (getSourceMob() != null && getSourceMob() instanceof ServerPlayer) {
             TriggerEvents.TNT_DESTROY_EVENT.invoker().onTNTDestroy((ServerPlayer) getSourceMob(), blockState, blockPos);
         }

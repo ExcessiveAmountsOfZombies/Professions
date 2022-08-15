9fb266ce06a5d89a6622f6476d3d5d06d1951b92
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2022-08-15 13:19:43 -0500

    fix the payment issue

--- src/main/java/com/epherical/professions/profession/rewards/builtin/PaymentReward.java
+++ src/main/java/com/epherical/professions/profession/rewards/builtin/PaymentReward.java
@@ -21,12 +21,11 @@ import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.level.storage.loot.Serializer;
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
 import java.util.UUID;
 
-public record PaymentReward(double amount, @Nullable Currency currency) implements Reward {
+public record PaymentReward(double amount, ResourceLocation currency) implements Reward {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     @Override
@@ -40,7 +39,7 @@ public record PaymentReward(double amount, @Nullable Currency currency) implemen
         if (economy != null) {
             UUID playerID = context.getParameter(ProfessionParameter.THIS_PLAYER).getUuid();
             UniqueUser user = economy.getOrCreatePlayerAccount(playerID);
-            Currency currency = this.currency;
+            Currency currency = economy.getCurrency(currency());
             if (ProfessionConfig.overrideCurrencyID) {
                 currency = economy.getCurrency(new ResourceLocation(ProfessionConfig.overriddenCurrencyID));
                 if (currency == null) {
@@ -48,6 +47,9 @@ public record PaymentReward(double amount, @Nullable Currency currency) implemen
                     LOGGER.warn("And change 'overrideCurrencyID' to false!");
                     throw new RuntimeException("payment could not be processed.");
                 }
+            }
+            if (currency == null) {
+                throw new RuntimeException("payment could not be processed.");
             }
             if (user != null) {
                 if (amount > 0) {
@@ -74,26 +76,15 @@ public record PaymentReward(double amount, @Nullable Currency currency) implemen
         @Override
         public void serialize(JsonObject json, PaymentReward value, JsonSerializationContext serializationContext) {
             json.addProperty("amount", value.amount);
-            if (value.currency != null) {
-                json.addProperty("currency", value.currency.getIdentity());
-            } else {
-                json.addProperty("currency", "eights_economy:dollars");
-            }
+            json.addProperty("currency", value.currency.toString());  json.addProperty("currency", "eights_economy:dollars");
         }
 
         @Override
         public PaymentReward deserialize(JsonObject json, JsonDeserializationContext serializationContext) {
             double amount = GsonHelper.getAsDouble(json, "amount");
-            Currency currency = null;
-            if (CommonPlatform.platform.economy() != null) {
-                String currencyString = GsonHelper.getAsString(json, "currency");
-                currency = CommonPlatform.platform.economy().getCurrency(new ResourceLocation(currencyString));
-                if (currency == null) {
-                    LOGGER.warn("PaymentReward used was a null currency {}. Using default currency of {}", currencyString, CommonPlatform.platform.economy().getDefaultCurrency().getIdentity());
-                    currency = CommonPlatform.platform.economy().getDefaultCurrency();
-                }
-            }
-            return new PaymentReward(amount, currency);
+            String currencyString = GsonHelper.getAsString(json, "currency");
+            ResourceLocation resourceKey = new ResourceLocation(currencyString);
+            return new PaymentReward(amount, resourceKey);
         }
     }
 
@@ -109,7 +100,7 @@ public record PaymentReward(double amount, @Nullable Currency currency) implemen
 
         @Override
         public Reward build() {
-            return new PaymentReward(amount, currency);
+            return new PaymentReward(amount, new ResourceLocation(currency.getIdentity()));
         }
     }
 }

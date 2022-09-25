535c188702e02fb7b068ee422be081c5aac94026
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2022-09-25 16:09:06 -0500

    fix this too

--- src/main/java/com/epherical/professions/profession/unlock/builtin/EquipmentUnlock.java
+++ src/main/java/com/epherical/professions/profession/unlock/builtin/EquipmentUnlock.java
@@ -119,7 +119,7 @@ public class EquipmentUnlock extends AbstractLevelUnlock<Item> {
 
         @Override
         public Unlock<Item> build() {
-            return new ToolUnlock(items, level);
+            return new EquipmentUnlock(items, level);
         }
     }
 
